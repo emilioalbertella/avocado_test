1372fabcf28c189b3067ed815f34c5daf2d22c3c
emilio <emilio.albertella@filoblu.com>
2024-11-24 16:12:55 +0100

    feature/db_dev

---
+++ database/migrations/2024_11_24_140450_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('users_id')->nullable();
+            $table->string('customer_email');
+            $table->string('customer_name');
+            $table->string('customer_address');
+            $table->string('description');
+            $table->timestamps();
+
+            $table->foreign('users_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
